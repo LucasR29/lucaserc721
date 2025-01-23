3a0b6f9fae6f091e14e1d992158afd6ccca302b7
LucasR29 <lrigon2904@gmail.com>
2025-01-22 23:54:00 -0300

    a

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,28 +1,32 @@
 import hre from "hardhat";
 import { privateKeyToAccount } from "viem/accounts";
 import { createWalletClient, http } from 'viem';
-import { hardhat } from 'viem/chains';
+import { hardhat, polygonAmoy } from 'viem/chains';
 
 export async function main() {
     console.log('Deploying SimpleOwnable...');
 
-    // Criar wallet client
+    const PRIVATE_KEY = "0xf2554f0416dcd829a7cb4512c96fc325e98831ac33c9c12d00f9a0a225fcb01a";
+    const account = privateKeyToAccount(PRIVATE_KEY);
+    
     const walletClient = createWalletClient({
-        chain: hardhat,
-        transport: http()
+        account,
+        chain: polygonAmoy,
+        transport: http("https://rpc-amoy.polygon.technology")
     });
 
-    const ownerAddress = "0xE7796aE4C33669447d12f01bD09cB2a6f0bfdFfC";
     const contract = await hre.viem.deployContract("SimpleOwnable", ["0xE7796aE4C33669447d12f01bD09cB2a6f0bfdFfC"]);
 
     console.log(`Contract SimpleOwnable deployed to: ${contract.address}`);
 
-    const message = await contract.write.setMessage(
-        ["Hello World"],
-        {
-            account: privateKeyToAccount("0xf2554f0416dcd829a7cb4512c96fc325e98831ac33c9c12d00f9a0a225fcb01a"),
-        }
-    );
+    const hash = await walletClient.writeContract({
+        address: contract.address,
+        abi: contract.abi,
+        functionName: 'setMessage',
+        args: ['Hello World']
+    });
+
+    console.log(`Transaction hash: ${hash}`);
 }
 
 main()
