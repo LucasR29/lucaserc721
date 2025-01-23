2bb4e19bf9932f6bdaf781a4ed9c2ebafd54b2e5
LucasR29 <lrigon2904@gmail.com>
2025-01-22 23:05:42 -0300

    a

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,5 +1,4 @@
 import hre from "hardhat";
-import { PK1 } from "../hardhat.anyflow.config";
 
 export async function main() {
     console.log('Deploying SimpleOwnable...');
@@ -8,7 +7,7 @@ export async function main() {
     const contract = await hre.viem.deployContract("SimpleOwnable", [ownerAddress]);
 
     const hash = await contract.write.transferOwnership(["0x0C86714619Ee1Ba9C4164cAAC7988d02538A6264"], {
-        account: "0xE7796aE4C33669447d12f01bD09cB2a6f0bfdFfC",
+        account: "0xf2554f0416dcd829a7cb4512c96fc325e98831ac33c9c12d00f9a0a225fcb01a",
     });
 
     console.log(`Transaction hash: ${hash}`);
